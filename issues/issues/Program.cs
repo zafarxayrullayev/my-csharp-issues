5eec0637a623a9c432904aba8473737e984a0131
zafarxayrullayev <xayrullayevzafar49@gamil.com>
2022-11-21 12:46:05 +0500

    worked 1st issue

---
+++ issues/issues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace issues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны положительные числа A и B(A > B). На отрезке длины A размещено максимально
+            //возможное количество отрезков длины B(без наложений).
+            //Не используя операции умножения и деления, найти длину незанятой части отрезка A.
+
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());    
+            b = Convert.ToInt32(Console.ReadLine());
+            int sumB = 0;
+            int shart = b;
+            while (shart < a)
+            {
+                sumB += b;
+                shart += b;
+            }
+            Console.WriteLine(a - sumB);
+        }
+    }
+}
